66bea300c8ed837e1cb3fc604bac686d5f6a28e3
Marcos Ulloa <marcosulloamata@gmail.com>
2018-08-12 03:05:42 -0600

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDX58bT78qGay_FPcOG0nlk6jn26jXfau4",
+    authDomain: "mallvirtual-7c4f9.firebaseapp.com",
+    databaseURL: "https://mallvirtual-7c4f9.firebaseio.com",
+    projectId: "mallvirtual-7c4f9",
+    storageBucket: "mallvirtual-7c4f9.appspot.com",
+    messagingSenderId: "864981266182"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDX58bT78qGay_FPcOG0nlk6jn26jXfau4",
+    authDomain: "mallvirtual-7c4f9.firebaseapp.com",
+    databaseURL: "https://mallvirtual-7c4f9.firebaseio.com",
+    projectId: "mallvirtual-7c4f9",
+    storageBucket: "mallvirtual-7c4f9.appspot.com",
+    messagingSenderId: "864981266182"
+  }
 };
 
 /*
